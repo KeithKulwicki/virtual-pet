065bb058c27fc4dcb0643acc05200c5aac18deda
Keith <Keith.Kulwicki@outlook.com>
2017-05-26 21:18:53 -0400

    v1

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class VirtualPet {
-	// attributes 
+	// attributes
 	int hungerLevel = 10;
 	int thirstLevel = 10;
 	int boredLevel = 10;
@@ -9,45 +9,61 @@ public class VirtualPet {
 
 	Random generator = new Random();
 
-	int getHunger() {return hungerLevel;}
-	int feed() {return hungerLevel = 0;}
-	int getThirst() {return thirstLevel;}
-	int water() {return thirstLevel = 0;}
-	int getBored() {return boredLevel;}
-	int play() {
-		hungerLevel += 5;// play = more hungey
-		thirstLevel += 5;// play = more thirsty
-		sleepyLevel += 5;// play = more sleepy
-		return boredLevel = 0;
+	int getHunger() {
+		return hungerLevel;
 	}
 
-	int getSleepy() {return sleepyLevel;}
-	int bed() {return sleepyLevel = 0;}
+	int feed() {
+		return hungerLevel = 0;
+	}// if eat, hunger = 0
 
-// Tick/make time pass
-	int getHungerTick() 
-	{
+	int getThirst() {
+		return thirstLevel;
+	}
+
+	int water() {
+		return thirstLevel = 0;
+	}// if drink, thirst = 0
+
+	int getBored() {
+		return boredLevel;
+	}
+
+	int play() {
+		hungerLevel += 5;// play = more hungry
+		thirstLevel += 5;// play = more thirsty
+		sleepyLevel += 5;// play = more sleepy
+		return boredLevel = 0; // if play, bored = 0
+	}
+
+	int getSleepy() {
+		return sleepyLevel;
+	}
+
+	int rest() {
+		return sleepyLevel = 0;
+	}// if rest, sleepy = 0
+
+	// Tick/make time pass
+	int getHungerTick() {
 		int tick = generator.nextInt(5);
 		hungerLevel += tick;
 		return hungerLevel;
 	}
 
-	int getThirstTick() 
-	{
+	int getThirstTick() {
 		int tick = generator.nextInt(5);
 		thirstLevel += tick;
 		return thirstLevel;
 	}
 
-	int getBoredTick() 
-	{		
+	int getBoredTick() {
 		int tick = generator.nextInt(5);
 		boredLevel += tick;
 		return boredLevel;
 	}
 
-	int getSleepyTick() 
-	{
+	int getSleepyTick() {
 		int tick = generator.nextInt(5);
 		sleepyLevel += tick;
 		return sleepyLevel;
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -13,9 +13,9 @@ public class VirtualPetApp {
 
 		int choice = 0;
 		System.out.println("HI! I'm Agador Spartacus, your virtual pet!");
-
+		System.out.println(""); System.out.println("");
 		do {
-			//get current status values, print user input
+			// get current status values, print user input
 			hungerLevel = agador.getHunger();
 			thirstLevel = agador.getThirst();
 			boredLevel = agador.getBored();
@@ -32,7 +32,8 @@ public class VirtualPetApp {
 			System.out.println("3: Tickle Agador Spartacus");
 			System.out.println("4: Beddy-by-boo time for Agador Spartacus");
 			System.out.println("5: Ignore Agador Spartacus");
-			System.out.println("6: Take Agador Spartacus back to the pet store");
+			System.out.println("6: Take Agador Spartacus back to the virtual pet store");
+			System.out.println("");
 			System.out.println("Choose and press 'Enter':");
 
 			choice = scanner.nextInt();
@@ -59,7 +60,7 @@ public class VirtualPetApp {
 				System.out.println("");
 				System.out.println("");
 			} else if (choice == 4) {
-				sleepyLevel = agador.bed();
+				sleepyLevel = agador.rest();
 				System.out.println("");
 				System.out.println("");
 				System.out.println("Nighty-night. SNOOOORE! SNOOOORE!");
@@ -67,12 +68,12 @@ public class VirtualPetApp {
 				System.out.println("");
 			}
 
-			 /* if "ignore", each attribute calls random number tick method*/
-			
+			/* if "ignore", each attribute calls random number tick method */
+
 			else if (choice == 5) {
 				System.out.println("");
 				System.out.println("");
-				System.out.println("HAY! Look at me! Look at me! Love me! Love me!");
+				System.out.println("HAY! Look at me. LOOK AT ME! Love me. LOVE ME!");
 				System.out.println("");
 				System.out.println("");
 				hungerLevel = agador.getHungerTick();
@@ -80,9 +81,9 @@ public class VirtualPetApp {
 				boredLevel = agador.getBoredTick();
 				sleepyLevel = agador.getSleepyTick();
 			}
-			//If xLevel 20 or more, virtual pet begs
+			// If xLevel 20 or more, virtual pet begs
 			if (hungerLevel > 19) {
-				System.out.println("Agador Spartacus is starving.");
+				System.out.println("Agador Spartacus is weak from hunger.");
 				System.out.println("");
 				System.out.println("");
 			}
@@ -97,15 +98,39 @@ public class VirtualPetApp {
 				System.out.println("");
 			}
 			if (sleepyLevel > 19) {
-				System.out.println("YAAAAAAAWN! Agador Spartacus is soooooo tired");
+				System.out.println("YAAAAAAAWN! Agador Spartacus...tired.");
 				System.out.println("");
 				System.out.println("");
 			}
 		} while (choice != 6);
 
-		System.out.println("Goodbye. Adieu. Auf wiedersehen. Gesundheit.");
-		System.out.println("Agador Spartacus didn't like it here anyway!");
-		System.out.println("Snif. Snif. *Agador Spartacus begins inconsolably weeping*");
+		System.out.println("Well I'm gonna to go then!");
+		System.out.println("And I don't need any of this.");
+		System.out.println("I don't need this stuff, and I don't need YOU.");
+		System.out.println("I don't need ANYTHING.");
+		System.out.println("Except this. [picks up an ashtray]");
+		System.out.println("And that's the only thing I need is THIS. I don't need this or this. Just this ashtray...");
+		System.out.println("And this paddle game.");
+		System.out.println("The ashtray and the paddle game and that's all I need...");
+		System.out.println("And this remote control.");
+		System.out.println("The ashtray, the paddle game, and the remote control, and that's all I need...");
+		System.out.println("And these matches. ");
+		System.out.println("The ashtray, and these matches, and the remote control, and the paddle ball...");
+		System.out.println("And this lamp. ");
+		System.out.println("The ashtray, this paddle game, and the remote control, and the lamp, ");
+		System.out
+				.println("and that's all I need. And that's ALL I need too. I don't need one other thing, not one...");
+		System.out.println(
+				"I need this. - The paddle game and the chair, and the remote control, and the matches for sure.");
+		System.out.println("Well what are you looking at? What do you think I'm some kind of a jerk or something!");
+		System.out.println("And this. That's all I need.");
+		System.out.println("[Agador Spartacus walking outside]");
+		System.out.println("The ashtray, the remote control, the paddle game, and this magazine, and the chair.");
+		System.out.println("[Agador Spartacus outside now] And I don't need one other thing...");
+		System.out.println("except my dog.");
+		System.out.println("[dog growls at Agador Spartacus]");
+		System.out.println("I don't need my dog!");
+
 		scanner.close();
 		System.exit(0);
 
